f67b41391f7a7440a39852343ddc6ef29617770e
darkohedgehog <zivic.darko79@gmail.com>
2023-05-11 22:20:54 +0200

    Adding a ErrorBoundry.js file

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Card from "./Card";
 
-const CardList = ({robots}) => {    
+const CardList = ({robots}) => {  
+   
     return (
         <div>
          {
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 import CardList from "../components/CardList";
 //import { robots } from "./robots";
 import SearchBox from "../components/SearchBox";
-import "./App.css";
+import "../containers/App.css";
 import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
 
 //import { render } from "@testing-library/react";
 
@@ -40,7 +41,9 @@ render() {
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
+          <ErrorBoundry>
         <CardList robots={filteredRobots} />
+        </ErrorBoundry>
         </Scroll>
       </div>
     );
